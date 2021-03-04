7ef60fd5bf6189113edf4027f906381722e9cdaa
saidakyuz <saidakyuz@gmail.com>
2021-03-04 19:43:37 +0100

    blog created with image,title,authorName,creationDate

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
-import {useState, useEffect} from 'react';
-import {client} from './client';
-import Blogpost from './Components/Blogpost';
+import { useState, useEffect } from 'react';
+import { client } from './client';
+import BlogPosts from './Components/BlogPosts';
 import './App.css';
 
 function App() {
-  const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, setBlogPosts] = useState([]);
 
-useEffect(() => {
-    client.getEntries({ content_type: 'blogPost' })
-        .then((response) => {
-            console.log(response)
-            setBlogPosts(response.items)
-        })
-        .catch(console.error)
-}, []);
-console.log(process);
+    useEffect(() => {
+        client.getEntries({ content_type: 'blogPost' })
+            .then((response) => {
+                console.log(response.items);
+                setBlogPosts(response.items);
+            })
+            .catch(console.error)
+    }, []);
+    console.log(process);
 
-  return (
-    <div className="App">
-                <div className='container'>
-                    <header>
-                        <div className='wrapper'>
-                            <span className='logo'>React and Contentful</span>
-                        </div>
-                    </header>
-                    <main>
-                        <div className='wrapper'>
-                            <Blogpost />
-                        </div>
-                    </main>
-                </div>
+    return (
+        <div className="App">
+            <div className='container'>
+                <header>
+                    <div className='wrapper'>
+                        <span className='logo'>React and Contentful</span>
+                    </div>
+                </header>
+                <main>
+                    <div className='wrapper'>
+                        <BlogPosts posts={blogPosts} />
+                    </div>
+                </main>
             </div>
-  );
+        </div>
+    );
 }
 
 export default App;
