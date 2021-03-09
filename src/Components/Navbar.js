b6f2c44a1ebc32fd13fc10dd8cffc674cf8aa5d2
Matchu Pitchu <77695211+MatchuPitchu@users.noreply.github.com>
2021-03-09 09:44:03 +0100

    Navbar Update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChild } from '@fortawesome/free-solid-svg-icons';
+import { faChild, faRunning, faBiking, faTableTennis, faSwimmer, faFutbol, faSkiing } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     
@@ -10,9 +10,21 @@ const Navbar = () => {
         <nav className="navbar navbar-light bg-light">
             <div className="container-fluid">
                 <NavLink className="navbar-brand" to="./">
-                   <FontAwesomeIcon icon={faChild} />
-                   <div>The Sports Blog</div>
+                    <div className="d-flex justify-content-between">
+                        <FontAwesomeIcon icon={faChild} />
+                        <FontAwesomeIcon icon={faRunning} />
+                        <FontAwesomeIcon icon={faBiking} />
+                        <FontAwesomeIcon icon={faTableTennis} />
+                        <FontAwesomeIcon icon={faSwimmer} />
+                        <FontAwesomeIcon icon={faFutbol} />
+                        <FontAwesomeIcon icon={faSkiing} />
+                    </div>
+                    <div>The Blog about Sport in Life</div>
                 </NavLink>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Search your topic" aria-label="Search" />
+                    <button className="btn btn-secondary my-btn" type="submit">Search</button>
+                </form>
             </div>
         </nav>
         </>
